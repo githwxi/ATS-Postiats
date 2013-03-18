65644e03b70bd941bd6b7e46128115b0ee954b9c
Hongwei Xi <gmhwxi@gmail.com>
2013-03-18 11:07:22 -0400

    Updating: adding pats_ccomp_memallc_gcbdw.h

--- ccomp/runtime/pats_ccomp_memalloc_gcbdw.h
+++ ccomp/runtime/pats_ccomp_memalloc_gcbdw.h
@@ -41,94 +41,48 @@ extern
 int fprintf (FILE *stream, const char *format, ...) ;
 */
 /* ****** ****** */
-//
-// stdlib.h
-//
-extern void exit (int code) ;
-//
-extern void free (void *ptr) ;
-extern void *malloc (size_t bsz) ;
-extern void *calloc(size_t asz, size_t tsz) ;
-extern void *realloc(void *ptr, size_t bsz) ;
-//
+/*
+#include <stdlib.h>
+*/
+extern void exit (int code) ; 
+
 /* ****** ****** */
-//
+/*
+#include <gc.h>
+*/
+extern void GC_free (void *ptr) ;
+extern void *GC_malloc(size_t bsz) ;
+
+/* ****** ****** */
+
 ATSinline()
 atsvoid_t0ype
-atsruntime_mfree_libc
-  (atstype_ptr ptr) { free(ptr) ; return ; }
-// end of [atsruntime_mfree_libc]
+atsruntime_mfree_gcbdw
+  (atstype_ptr ptr) { GC_free(ptr) ; return ; }
+// end of [atsruntime_mfree_gcbdw]
 
 /* ****** ****** */
 
 ATSinline()
 atstype_ptr
-atsruntime_malloc_libc
-  (atstype_size bsz) { return malloc(bsz) ; }
-// end of [atsruntime_malloc_libc]
+atsruntime_malloc_gcbdw
+  (atstype_size bsz) { return GC_malloc(bsz) ; }
+// end of [atsruntime_malloc_gcbdw]
 
 ATSinline()
 atstype_ptr
-atsruntime_malloc_libc_exn
+atsruntime_malloc_gcbdw_exn
   (atstype_size bsz)
 {
   atstype_ptr p ;
-  p = atsruntime_malloc_libc(bsz) ;
+  p = atsruntime_malloc_gcbdw(bsz) ;
   if (!p) {
-    fprintf(stderr, "exit(ATS): atsruntime_malloc_libc_exn: [malloc] failed.\n") ;
+    fprintf(stderr, "exit(ATS): atsruntime_malloc_gcbdw_exn: [malloc] failed.\n") ;
     exit(1) ;
   } // end of [if]
   return (p) ;
-} /* end of [atsruntime_malloc_libc_exn] */
+} /* end of [atsruntime_malloc_gcbdw_exn] */
 
 /* ****** ****** */
 
-ATSinline()
-atstype_ptr
-atsruntime_calloc_libc
-  (atstype_size asz, atstype_size tsz) { return calloc(asz, tsz) ; }
-// end of [atsruntime_calloc_libc]
-
-ATSinline()
-atstype_ptr
-atsruntime_calloc_libc_exn
-(
-  atstype_size asz, atstype_size tsz
-)
-{
-  atstype_ptr p ;
-  p = atsruntime_calloc_libc(asz, tsz) ;
-  if (!p) {
-    fprintf(stderr, "exit(ATS): atsruntime_calloc_libc_exn: [calloc] failed.\n") ;
-    exit(1) ;
-  } // end of [if]
-  return (p) ;
-} /* end of [atsruntime_calloc_libc_exn] */
-
-/* ****** ****** */
-
-ATSinline()
-atstype_ptr
-atsruntime_realloc_libc
-  (atstype_ptr ptr, atstype_size bsz) { return realloc(ptr, bsz) ; }
-// end of [atsruntime_realloc_libc]
-
-ATSinline()
-atstype_ptr
-atsruntime_realloc_libc_exn
-(
-  atstype_ptr ptr, atstype_size bsz
-)
-{
-  atstype_ptr p ;
-  p = atsruntime_realloc_libc(ptr, bsz) ;
-  if (!p) {
-    fprintf(stderr, "exit(ATS): atsruntime_realloc_libc_exn: [realloc] failed.\n") ;
-    exit(1) ;
-  } // end of [if]
-  return (p) ;
-} /* end of [atsruntime_realloc_libc_exn] */
-
-/* ****** ****** */
-
-/* end of [pats_ccomp_memalloc_libc.h] */
+/* end of [pats_ccomp_memalloc_gcbdw.h] */
