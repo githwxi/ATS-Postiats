2baf48843705bb8afb85332d2a44d641c211e8ec
Hongwei Xi <gmhwxi@gmail.com>
2014-10-16 01:07:59 -0400

    Updating: improving ats2langweb/patsoptaas

--- doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyRight/Examples.php
+++ doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyRight/Examples.php
@@ -9,14 +9,27 @@ style="margin-top:8px;padding:8px;"
 <h2 style="display:inline;">More examples in ATS</h2>
 
 <ul>
+
 <li>
 <a
 href="http://atslangweb-postiats.rhcloud.com/SERVER/MYCODE/Patsoptaas_serve.php?mycode_url=https://raw.githubusercontent.com/githwxi/ATS-Postiats-contrib/master/projects/SMALL/JSclock/myclock1-2.dats">Drawing a clock on canvas</a>
 </li>
+
 <li>
 <a
 href="http://atslangweb-postiats.rhcloud.com/SERVER/MYCODE/Patsoptaas_serve.php?mycode_url=https://raw.githubusercontent.com/githwxi/ATS-Postiats/master/doc/BOOK/INT2PROGINATS/CODE/CHAP_FUNCTION/JS/queens.dats">Solving the 8-queen puzzle</a>
 </li>
+
+<li>
+A
+<a
+href="http://atslangweb-postiats.rhcloud.com/SERVER/MYCODE/Patsoptaas_serve.php?mycode_url=https://raw.githubusercontent.com/githwxi/ATS-Postiats/master/doc/EXAMPLE/ARITH/gcd.dats">proof</a>
+of
+<a
+href="http://en.wikipedia.org/wiki/B%C3%A9zout%27s_identity">Bézout's identity</a>
+in ATS
+</li>
+
 </ul>
 
 <hr></hr>
