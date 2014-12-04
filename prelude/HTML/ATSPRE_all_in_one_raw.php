9d9935a68672da2dbe44244bb1e3aa0c57775fef
Hongwei Xi <gmhwxi@gmail.com>
2014-12-04 14:09:21 -0500

    Updating: minorly improving

---
+++ prelude/HTML/ATSPRE_all_in_one_raw.php
@@ -0,0 +1,91 @@
+<?php
+//
+include './../fixity.ats';
+//
+include './../basics_pre.sats';
+include './../basics_sta.sats';
+include './../basics_dyn.sats';
+include './../basics_gen.sats';
+//
+include './../macrodef.sats';
+include './../lmacrodef.sats';
+//
+// SATS
+//
+include './../SATS/integer.sats';
+include './../SATS/pointer.sats';
+include './../SATS/memory.sats';
+//
+include './../SATS/bool.sats';
+include './../SATS/char.sats';
+include './../SATS/float.sats';
+include './../SATS/string.sats';
+include './../SATS/strptr.sats';
+//
+include './../SATS/integer_ptr.sats';
+include './../SATS/integer_fixed.sats';
+//
+include './../SATS/unsafe.sats';
+include './../SATS/checkast.sats';
+//
+include './../SATS/filebas.sats';
+include './../SATS/intrange.sats';
+//
+include './../SATS/list.sats';
+include './../SATS/list_vt.sats';
+//
+include './../SATS/option.sats';
+include './../SATS/option_vt.sats';
+//
+include './../SATS/stream.sats';
+include './../SATS/stream_vt.sats';
+//
+include './../SATS/array.sats';
+include './../SATS/arrayptr.sats';
+include './../SATS/arrayref.sats';
+//
+include './../SATS/matrix.sats';
+include './../SATS/matrixptr.sats';
+include './../SATS/matrixref.sats';
+//
+// DATS
+//
+include './../DATS/basics.dats';
+//
+include './../DATS/integer.dats';
+include './../DATS/pointer.dats';
+include './../DATS/memory.dats';
+//
+include './../DATS/bool.dats';
+include './../DATS/char.dats';
+include './../DATS/float.dats';
+include './../DATS/string.dats';
+include './../DATS/strptr.dats';
+//
+include './../DATS/unsafe.dats';
+include './../DATS/checkast.dats';
+//
+include './../DATS/filebas.dats';
+include './../DATS/intrange.dats';
+//
+include './../DATS/list.dats';
+include './../DATS/list_vt.dats';
+//
+include './../DATS/option.dats';
+include './../DATS/option_vt.dats';
+//
+include './../DATS/stream.dats';
+include './../DATS/stream_vt.dats';
+//
+include './../DATS/array.dats';
+include './../DATS/array_prf.dats';
+include './../DATS/arrayptr.dats';
+include './../DATS/arrayref.dats';
+//
+include './../DATS/matrix.dats';
+include './../DATS/matrixptr.dats';
+include './../DATS/matrixref.dats';
+//
+?>
+
+(* end of [ATSPRE_all_in_one.raw] *)
