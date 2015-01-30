91a75b501c9a9b3cf48de5f2d96d97be20e5e627
Hongwei Xi <gmhwxi@gmail.com>
2015-01-30 01:37:01 -0500

    Updating: very minorly improving

--- doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyRight/Resources.php
+++ doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyRight/Resources.php
@@ -2,8 +2,7 @@
 style="margin-top:8px;padding:8px;"
 ><!--div-->
 
-<h2
- style="display:inline;">More resources</h2>
+<h2 style="display:inline;">More resources</h2>
 
 <hr></hr>
 
@@ -13,7 +12,6 @@ style="margin-top:8px;padding:8px;"
 </a></b><br>
 An ATS plugin for <a href="https://www.jetbrains.com/idea/">IntelliJ IDEA</a><br>
 by Brandon Barker
-
 <hr></hr>
 
 <b>
@@ -25,8 +23,13 @@ An ATS syntax highlighter for <a href="http://www.sublimetext.com">SublimeText 2
 by Hanwen (Steinway) Wu
 <hr></hr>
 
-<b><a href="https://github.com/Hibou57/ats2-language-support">ats2-language-support</a></b><br>
-ATS2 language support and integration (for now, for Linux with Freedesktop and GtkSourceView)
+<b>
+<a
+ href="https://github.com/Hibou57/ats2-language-support">Ats2-language-support</a>
+</b><br>
+ATS2 language support and integration
+(currently for Linux with Freedesktop and GtkSourceView)<br>
+by Yannick Duchêne
 <hr></hr>
 
 </div>
