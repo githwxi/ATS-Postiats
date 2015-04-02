70a7d50ea4147cb25ff51a8fadb786d8869f8910
Hongwei Xi <gmhwxi@gmail.com>
2015-04-02 18:46:15 -0400

    Updating: very minorly improving

--- doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyLContent/Downloads.php
+++ doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyLContent/Downloads.php
@@ -310,7 +310,7 @@ violations.
 <ul>
 <li>
 The current <em>stable</em> release of ATS2-include is at
-<a href="http://sourceforge.net/projects/ats2-lang/files/ats2-lang/ats2-postiats-0.1.7/.">ATS2-include-0.1.7</a>.
+<a href="http://sourceforge.net/projects/ats2-lang/files/ats2-lang/ats2-postiats-0.1.10/.">ATS2-include-0.1.10</a>.
 </li>
 </ul>
 
