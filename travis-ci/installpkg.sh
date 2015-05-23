bcd333455d95a6edd6c8ea09c3c9bae240196f0c
Hongwei Xi <gmhwxi@gmail.com>
2015-05-23 01:35:59 -0400

    Updating: very minorly improving

--- travis-ci/installpkg.sh
+++ travis-ci/installpkg.sh
@@ -4,6 +4,34 @@
 
 cd ${HOME}
 
+###### for OSX ######
+
+is_osx=`expr "${TRAVIS_OS_NAME}" : "osx"`;
+
+# echo "is_osx = ${is_osx}"
+
+if
+  expr ${is_osx} > 0
+then
+#
+# export CC=gcc-4.8
+# export GCC=gcc-4.8
+#
+  brew install gmp
+  brew install bdw-gc
+  brew install pcre
+  brew install glib
+  brew install cairo
+  brew install gtk+3
+  brew install libev
+  brew install jansson
+  brew install caskroom/cask/brew-cask
+  brew cask install xquartz
+#
+  export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig:$PKG_CONFIG_PATH
+#
+fi
+
 ###### for LINUX ######
 
 is_linux=`expr "${TRAVIS_OS_NAME}" : "linux"`;
