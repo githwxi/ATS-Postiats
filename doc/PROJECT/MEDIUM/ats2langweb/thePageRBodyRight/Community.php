2a9335409454ad016b94599e945bf235eb859372
Hongwei Xi <gmhwxi@gmail.com>
2015-06-24 03:49:41 -0400

    Updating: adding a news item

--- doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyRight/Community.php
+++ doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyRight/Community.php
@@ -6,6 +6,14 @@ style="margin-top:8px;padding:8px;"
 
 <hr></hr>
 
+ATS-2015-06-23:<br>
+<a
+ href="https://groups.google.com/forum/#!topic/ats-lang-users/AmeOXNUZ7Ik"
+>Tetris: Autoplay is supported</a><br>
+by <a href="http://www.cs.bu.edu/~hwxi">Hongwei Xi</a>
+
+<hr></hr>
+
 ATS-2015-06-09:<br>
 <a
  href="https://groups.google.com/forum/#!topic/ats-lang-users/j4ekLh2j6Zw"
