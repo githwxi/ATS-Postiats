927462a5464293805fe11701263d571664e21d97
Hongwei Xi <gmhwxi@gmail.com>
2016-11-19 11:15:00 -0500

    Updating: very very minorly

--- doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyRight/Community.php
+++ doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyRight/Community.php
@@ -6,6 +6,14 @@ style="margin-top:8px;padding:8px;"
 
 <hr></hr>
 
+ATS-2016-11-18:<br>
+<a
+ href="https://groups.google.com/forum/#!topic/ats-lang-users/ewu1F-oL5vg"
+>Compiling ATS via patsopt with g++ </a><br>
+by <a href="http://www.cs.bu.edu/~hwxi">Hongwei Xi</a>
+
+<hr></hr>
+
 ATS-2016-10-15:<br>
 <a
  href="https://groups.google.com/forum/#!topic/ats-lang-users/qGw-xpQVuOM"
