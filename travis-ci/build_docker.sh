7f78d3fcaa2d4033cb0c42ccbfbab40dc315db3d
Hanwen Wu <steinwaywhw@gmail.com>
2017-01-19 12:39:32 -0500

    debug docker

--- travis-ci/build_docker.sh
+++ travis-ci/build_docker.sh
@@ -7,12 +7,16 @@ docker build -f Dockerfile -t $REPO:$TRAVIS_COMMIT --build-arg ATSVER=${ATSVER} 
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then 
 	if [ -z "$TRAVIS_TAG" ]; then 
+		echo "$REPO:git"
 		docker tag $REPO:$TRAVIS_COMMIT $REPO:git                 # steinwaywhw/ats:git
 	else 
+		echo "$REPO:$TRAVIS_TAG"
 		docker tag $REPO:$TRAVIS_COMMIT $REPO:$TRAVIS_TAG         # steinwaywhw/ats:1.0
+		echo "$REPO:latest"
 		docker tag $REPO:$TRAVIS_COMMIT $REPO:latest              # steinwaywhw/ats:latest
 	fi
 else
+	echo "$REPO:$TRAVIS_BRANCH"
 	docker tag $REPO:$TRAVIS_COMMIT $REPO:$TRAVIS_BRANCH          # steinwaywhw/ats:branch
 fi
 
