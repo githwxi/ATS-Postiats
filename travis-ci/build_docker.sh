f7e8535d11bee1e54d3c1000ceb50d2b9b178044
Hanwen Wu <steinwaywhw@gmail.com>
2017-01-19 11:11:39 -0500

    try docker build

---
+++ travis-ci/build_docker.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+export REPO=steinwaywhw/ats
+export COMMIT=${TRAVIS_COMMIT::8}
+
+docker login -e ${DOCKER_EMAIL} -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
+docker build 
+	-f Dockerfile \
+	-t $REPO:$COMMIT \
+	--build-arg ATSVER=${ATSVER} \
+	.
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then 
+	if [ -z "$TRAVIS_TAG" ]; then 
+		docker tag $REPO:git                 # steinwaywhw/ats:git
+	else 
+		docker tag $REPO:$TRAVIS_TAG         # steinwaywhw/ats:1.0
+		docker tag $REPO:latest              # steinwaywhw/ats:latest
+	fi
+else
+	docker tag $REPO:$TRAVIS_BRANCH/git  # steinwaywhw/ats:branch/git
+fi
+
+docker push $REPO
+
+
--- travis-ci/install_contrib.sh
+++ travis-ci/install_contrib.sh
@@ -1,3 +1,57 @@
 #!/usr/bin/env sh
 
- git clone https://github.com/githwxi/ATS-Postiats-contrib.git ATS-Postiats-contrib
+git clone https://github.com/githwxi/ATS-Postiats-contrib
+
+echo "Skip the build until it is fixed."
+
+# ######
+# #
+# # For parsing constraints 
+# #
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/ATS-extsolve && make DATS_C)
+# #
+# # For building patsolve_z3
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/ATS-extsolve/ATS-extsolve-z3 && make build)
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/ATS-extsolve/ATS-extsolve-z3 && mv -f patsolve_z3 ${PATSHOME}/bin)
+# #
+# # For building patsolve_smt2
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/ATS-extsolve/ATS-extsolve-smt2 && make build)
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/ATS-extsolve/ATS-extsolve-smt2 && mv -f patsolve_smt2 ${PATSHOME}/bin)
+# #
+# ######
+# #
+# # For parsing C code
+# # generated from ATS source
+# #
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/CATS-parsemit && make DATS_C)
+# #
+# # For building atscc2js
+# #
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/CATS-atsccomp/CATS-atscc2js && make build)
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/CATS-atsccomp/CATS-atscc2js && mv -f atscc2js ${PATSHOME}/bin)
+# (cd ${PATSHOMERELOC}/contrib/libatscc/libatscc2js && make all && make all_in_one)
+# #
+# # For building atscc2py3
+# #
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/CATS-atsccomp/CATS-atscc2py3 && make build)
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/CATS-atsccomp/CATS-atscc2py3 && mv -f atscc2py3 ${PATSHOME}/bin)
+# (cd ${PATSHOMERELOC}/contrib/libatscc/libatscc2py3 && make all && make all_in_one)
+# #
+# # For building atscc2scm
+# #
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/CATS-atsccomp/CATS-atscc2scm && make build)
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/CATS-atsccomp/CATS-atscc2scm && mv -f atscc2scm ${PATSHOME}/bin)
+# (cd ${PATSHOMERELOC}/contrib/libatscc/libatscc2scm && make all && make all_in_one)
+# #
+# # For building atscc2clj
+# #
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/CATS-atsccomp/CATS-atscc2clj && make build)
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/CATS-atsccomp/CATS-atscc2clj && mv -f atscc2clj ${PATSHOME}/bin)
+# (cd ${PATSHOMERELOC}/contrib/libatscc/libatscc2clj && make all && make all_in_one)
+# #
+# # For building atscc2erl
+# #
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/CATS-atsccomp/CATS-atscc2erl && make build)
+# (cd ${PATSHOMERELOC}/projects/MEDIUM/CATS-atsccomp/CATS-atscc2erl && mv -f atscc2erl ${PATSHOME}/bin)
+# (cd ${PATSHOMERELOC}/contrib/libatscc/libatscc2erl && make all && make all_in_one)
+# (cd ${PATSHOMERELOC}/contrib/libatscc/libatscc2erl/Session && make all && make all_in_one)
