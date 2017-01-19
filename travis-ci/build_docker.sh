06d5caf51f35eb6732860f21ea96bf2625365c2d
Hanwen Wu <steinwaywhw@gmail.com>
2017-01-19 12:45:54 -0500

    fixed

--- travis-ci/build_docker.sh
+++ travis-ci/build_docker.sh
@@ -9,17 +9,21 @@ if [[ "$TRAVIS_BRANCH" == "master" ]]; then
 	if [ -z "$TRAVIS_TAG" ]; then 
 		echo "$REPO:git"
 		docker tag $REPO:$TRAVIS_COMMIT $REPO:git                 # steinwaywhw/ats:git
+		docker push $REPO:git
 	else 
 		echo "$REPO:$TRAVIS_TAG"
 		docker tag $REPO:$TRAVIS_COMMIT $REPO:$TRAVIS_TAG         # steinwaywhw/ats:1.0
+		docker push $REPO:$TRAVIS_TAG
 		echo "$REPO:latest"
 		docker tag $REPO:$TRAVIS_COMMIT $REPO:latest              # steinwaywhw/ats:latest
+		docker push $REPO:latest
 	fi
 else
 	echo "$REPO:$TRAVIS_BRANCH"
 	docker tag $REPO:$TRAVIS_COMMIT $REPO:$TRAVIS_BRANCH          # steinwaywhw/ats:branch
+	docker push $REPO:$TRAVIS_BRANCH
 fi
 
-docker push $REPO
+
 
 
--- travis-ci/install_utilities.sh
+++ travis-ci/install_utilities.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 cp ${ATSHOME}/config.h .
-make -C src cleanall
-make -C src/CBOOT/prelude
-make -C src/CBOOT/libc
-make -C src/CBOOT/libats
+# make -C src cleanall
+# make -C src/CBOOT/prelude
+# make -C src/CBOOT/libc
+# make -C src/CBOOT/libats
 make -C utils/libatsopt
 cp utils/libatsopt/libatsopt.a ${ATSHOME}/ccomp/lib
 make -C utils/libatsynmark
