e4e623710363214fe62f14103a1b883f070d1022
Hanwen Wu <steinwaywhw@gmail.com>
2017-01-18 21:23:14 -0500

    debug

--- travis-ci/install_ats1.sh
+++ travis-ci/install_ats1.sh
@@ -20,6 +20,9 @@ ATSLANGURL_github=http://ats-lang.github.io
 ######
 #
 echo ${PWD}
+echo ${HOME}
+echo ${ATSHOME}
+
 wget -q ${ATSLANGURL_github}/ATS-Anairiats/${ATSPACKTGZ}
 # wget -q ${ATSLANGURL_srcfg}/files/ats-lang/anairiats-latest/${ATSPACKTGZ}
 tar -zxf ${ATSPACKTGZ}
