26c437af2bc65a9cdfbaaf7991cae9fd869ebe71
Hongwei Xi <gmhwxi@gmail.com>
2017-02-04 09:29:08 -0500

    Updating: some minor reorganization

---
+++ npm-utils/contrib/libats-/hwxi/HTML-emscripten/CATS_JS/HTMLdocument.js
@@ -0,0 +1,85 @@
+/* ****** ****** */
+
+/**
+  HTML5/canvas-2d functions for ATS2
+
+  Author: Will Blair
+  Start Time: October 2013
+  Authoremail: wdblairATcsDOTbuDOTedu
+
+  Author: Hongwei Xi
+  Start Time: October 2013
+  Authoremail: hwxi AT cs DOT bu DOT edu
+ */
+
+/* ****** ****** */
+
+var atscntrb_html_emscripten_document =
+{
+//
+    $MyDocument: {
+	objcnt: 0,
+	contexts: {},
+	objadd: function (obj) {
+	    var idx = ++MyDocument.objcnt;
+	    MyDocument.contexts[idx] = obj;
+	    return idx;
+	}
+    },
+//
+    atscntrb_html_emscripten_document_encode:
+    function(obj) { return MyDocument.objadd(obj) ; },
+    atscntrb_html_emscripten_document_decode:
+    function(idx) { return MyDocument.contexts[idx] ; },
+//
+    atscntrb_html_emscripten_document_get_documentElement_clientWidth:
+    function () { return document.documentElement.clientWidth; },
+    atscntrb_html_emscripten_document_get_documentElement_clientHeight:
+    function () { return document.documentElement.clientHeight; },
+//
+    atscntrb_html_emscripten_document_element_free:
+    function (idx) { delete MyDocument.contexts[idx]; },
+    atscntrb_html_emscripten_document_event_free:
+    function (idx) { delete MyDocument.contexts[idx]; },
+//
+    atscntrb_html_emscripten_document_getElementById:
+    function (id) {
+	var id2 = Pointer_stringify(id);
+	return MyDocument.objadd(document.getElementById(id2));
+    },
+//
+    atscntrb_html_emscripten_document_element_get_width:
+    function (idx) { return MyDocument.contexts[idx].width; },
+    atscntrb_html_emscripten_document_element_get_height:
+    function (idx) { return MyDocument.contexts[idx].height; },
+//
+    atscntrb_html_emscripten_document_element_set_width:
+    function (idx, width) { MyDocument.contexts[idx].width = width; },
+    atscntrb_html_emscripten_document_element_set_height:
+    function (idx, height) { MyDocument.contexts[idx].height = height; },
+//
+    atscntrb_html_emscripten_document_element_get_value_int:
+    function (idx) {
+        var res = parseInt(MyDocument.contexts[idx].value);
+        if (isNaN(res)) { return -1000000; } else { return res; }
+    },
+//
+    atscntrb_html_emscripten_document_event_get_keyCode:
+    function (idx) { return MyDocument.contexts[idx].keyCode; },
+    atscntrb_html_emscripten_document_event_get_target:
+    function (idx) {
+        return MyDocument.objadd(MyDocument.contexts[idx].target);
+    }
+} ; // end of [atscntrb_html_emscripten_document]
+
+/* ****** ****** */
+
+autoAddDeps(atscntrb_html_emscripten_document, '$MyDocument');
+
+/* ****** ****** */
+
+mergeInto(LibraryManager.library, atscntrb_html_emscripten_document);
+
+/* ****** ****** */
+
+/* end of [HTMLdocument.js] */
