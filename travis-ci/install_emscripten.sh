e893d632396cbe3da009101fabb3cbf96448e46a
Hanwen Wu <steinwaywhw@gmail.com>
2017-02-24 11:19:17 -0500

    fix a bug

--- travis-ci/install_emscripten.sh
+++ travis-ci/install_emscripten.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 wget https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz
-tar -xvf emsdk-portable.tar.gz
+tar -xzf emsdk-portable.tar.gz
 rm emsdk-portable.tar.gz
 
 apt-get install -qq -y cmake default-jre
 
-cd emsdk-portable
+cd emsdk_portable
 ./emsdk update
 ./emsdk install latest
 ./emsdk activate latest
