8581ce8884a26af9e31361f1ed82fef697ffcd70
Hongwei Xi <gmhwxi@gmail.com>
2017-03-05 19:29:39 -0500

    Updating: initiating ATS2-0.3.4

--- doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyRight/Community.php
+++ doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyRight/Community.php
@@ -6,6 +6,14 @@ style="margin-top:8px;padding:8px;"
 
 <hr></hr>
 
+2017-03-05:<br>
+<a
+ href="https://groups.google.com/forum/#!topic/ats-lang-users/wtlb5CyMNA0"
+>ATS2-0.3.3 has been released</a><br>
+by <a href="http://www.cs.bu.edu/~hwxi">Hongwei Xi</a>
+
+<hr></hr>
+
 2017-02-12:<br>
 <a
  href="https://groups.google.com/forum/#!topic/ats-lang-users/y3ccbpFUKwE"
--- src/pats_basics.sats
+++ src/pats_basics.sats
@@ -216,7 +216,7 @@ PATS_MAJOR_VERSION 0
 #define
 PATS_MINOR_VERSION 3
 #define
-PATS_MICRO_VERSION 3
+PATS_MICRO_VERSION 4
 //
 // HX-2011-04-27: this is supported in Postiats:
 //
