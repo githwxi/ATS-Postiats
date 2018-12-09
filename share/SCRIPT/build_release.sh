ac578161ff9d70bdc94ba75b89c5bdfd3ba7acef
Hongwei Xi <gmhwxi@gmail.com>
2018-12-09 10:58:42 -0500

    Updating: more work on build_release.sh

--- share/SCRIPT/build_release.sh
+++ share/SCRIPT/build_release.sh
@@ -36,9 +36,15 @@
 # rm -rf /tmp/ATS-Postiats-contrib
 #
 ######
-
+#
 # PWD=$(pwd)
+#
+######
+
 PATSVERSION=$1
+
+######
+
 GIT=$(which git)
 ATSCC=$(which atscc)
 
@@ -47,7 +53,7 @@ ATSCC=$(which atscc)
 check_git() {
     if [ -x "$GIT" ] ; 
     then echo "$GIT found.";
-    else echo "$GIT not found." exit 1;
+    else echo "$GIT not found." && exit 1;
     fi
 }
 
@@ -56,7 +62,7 @@ check_git() {
 check_atscc() {
     if [ -x "$ATSCC" ] ; 
     then echo "$ATSCC found.";
-    else echo "$ATSCC not found." exit 1;
+    else echo "$ATSCC not found." && exit 1;
     fi
 }
 
@@ -69,16 +75,15 @@ export_pats() {
 
 ######
 
-clone_ATS_Postiats() {
+clone_Postiats() {
     $GIT clone --depth 1 \
 	 https://github.com/githwxi/ATS-Postiats.git \
 	|| (cd ATS-Postiats && $GIT pull origin master)
-        # https://github.com/githwxi/ATS-Postiats.git \
 }
 
 ######
 
-clone_ATS_Postiats_contrib() {
+clone_Postiats_contrib() {
     $GIT clone --depth 1 \
 	 https://github.com/githwxi/ATS-Postiats-contrib.git \
 	|| (cd ATS-Postiats-contrib && $GIT pull origin master)
@@ -115,28 +120,35 @@ check_ac_init_version() {
 ######
 
 checkout_build() {
-    (cd "$PATSHOME" && \
-	 $GIT checkout -b "tags/v${PATSVERSION}")
+    ( cd "$PATSHOME" && \
+	    $GIT checkout -b "tags/v${PATSVERSION}"
+    )
 }
 
 build_release_int() {
-    (cd "$PATSHOME" && \
-	 make -f Makefile_devl C3NSTRINTKND=intknd && \
-	 make -C src -f Makefile CBOOTint && \
-	 (cp ./bin/*_env.sh.in ./doc/DISTRIB/ATS-Postiats/bin/.) && \
-	 (cd ./doc/DISTRIB/ATS-Postiats && \
-	      sh ./autogen.sh && ./configure) && \
-	 make -C src/CBOOT/libc -f Makefile && \
-	 make -C src/CBOOT/libats -f Makefile && \
-	 make -C src/CBOOT/prelude -f Makefile && \
-	 make -C doc/DISTRIB -f Makefile atspackaging && \
-	 make -C doc/DISTRIB -f Makefile atspacktarzvcf_int
+    ( cd "$PATSHOME" &&
+	    make -C doc/DISTRIB -f Makefile_inclats tarzvcf
+    )
+    ( cd "$PATSHOME" && \
+	    make -f Makefile_devl C3NSTRINTKND=intknd && \
+	    make -C src -f Makefile CBOOTint && \
+	    (cp ./bin/*_env.sh.in ./doc/DISTRIB/ATS-Postiats/bin/.) && \
+	    (cd ./doc/DISTRIB/ATS-Postiats && \
+		    sh ./autogen.sh && ./configure) && \
+	    make -C src/CBOOT/libc -f Makefile && \
+	    make -C src/CBOOT/libats -f Makefile && \
+	    make -C src/CBOOT/prelude -f Makefile && \
+	    make -C doc/DISTRIB -f Makefile atspackaging && \
+	    make -C doc/DISTRIB -f Makefile atspacktarzvcf_int
     )
 }
 
 ######
 
 build_release_gmp() {
+    ( cd "$PATSHOME" &&
+	    make -C doc/DISTRIB -f Makefile_inclats tarzvcf
+    )
     ( cd "$PATSHOME" && \
 	    make -f Makefile_devl C3NSTRINTKND=gmpknd && \
 	    make -C src -f Makefile CBOOTgmp && \
@@ -153,10 +165,9 @@ build_release_gmp() {
 
 ######
 
-build_release_ext() {
-    ( cd "$PATSHOME" &&
-	    make -C doc/DISTRIB -f Makefile_inclats tarzvcf
-    )
+build_release_contrib() {
+    clone_Postiats_contrib
+
     ( cd "$PATSHOME" &&
 	    make -C doc/DISTRIB -f Makefile atscontribing && \
 	    make -C doc/DISTRIB -f Makefile atscontribtarzvcf
@@ -180,23 +191,22 @@ _usage() {
     echo "KIND"
     echo "intknd        ATS2-int: no need for gmplib"
     echo "gmpknd        ATS2-gmp: dependency on gmplib"
+    echo "contrib       ATS2-contrib: contributed packages"
     exit
 }
 
 
 main() {
 
-    [ -z "$1" ] && _usage "Please specify build VERSION and KIND"
     [ -z "$2" ] && _usage "Please specify build KIND"
+    [ -z "$1" ] && _usage "Please specify build VERSION"
 
     check_git 
     check_atscc
     export_pats
-    clone_ATS_Postiats
-    clone_ATS_Postiats_contrib
+    clone_Postiats
     check_version
     check_ac_init_version
-
     checkout_build
 
     case "$2" in
@@ -207,6 +217,8 @@ main() {
 	both)
 	    build_release_int && build_cleanall
 	    build_release_gmp && build_cleanall ;;
+	contrib)
+	    build_release_contrib && build_cleanall ;;
 	*) _usage ;; 
     esac
 
