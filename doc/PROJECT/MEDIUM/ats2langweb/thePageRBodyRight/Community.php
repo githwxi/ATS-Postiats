d5b91503f400f3433e73d70cbae455000a3b492f
Hongwei Xi <gmhwxi@gmail.com>
2020-08-02 23:41:55 -0400

    Updating: very very minorly

--- doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyRight/Community.php
+++ doc/PROJECT/MEDIUM/ats2langweb/thePageRBodyRight/Community.php
@@ -6,6 +6,14 @@ style="margin-top:8px;padding:8px;"
 
 <hr></hr>
 
+2019-08-02:<br>
+<a
+ href="https://groups.google.com/forum/#!topic/ats-lang-users/"
+>ATS2-0.4.1 has been released</a><br>
+by <a href="http://www.cs.bu.edu/~hwxi">Hongwei Xi</a>
+
+<hr></hr>
+
 2019-12-24:<br>
 <a
  href="https://groups.google.com/forum/#!topic/ats-lang-users/UcmMkpA5qDQ"
